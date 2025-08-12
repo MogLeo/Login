baf69e493c7b95e7ebf5c611dffb0c50b960688b
Leonardo Santos <137180866+MogLeo@users.noreply.github.com>
2025-08-12 14:46:18 -0300

    Add files via upload

---
+++ login.py
@@ -0,0 +1,9 @@
+usuario = input("Digite seu nome: ")
+senha = input("Digite a senha: ")
+
+if usuario == "Olegario" and senha == "234":
+    print("Bem-vindo, Olegario!")
+    print("👌👌👌")
+else:
+    print("Usuário ou senha incorretos. Tente novamente ")
+    print("🤐🤐🤐")
